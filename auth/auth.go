6f5c2d17f78577b06485484ccc1edfda4fdb7def
CyrusN <CyrusN@CyrusN.local>
2017-08-20 19:54:10 +0800

    update server/route
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Changes to be committed:
    	modified:   .gitignore
    	new file:   auth/auth.go
    	deleted:    model/testing.db
    	new file:   server/handlers.go
    	new file:   server/routes.go

---
+++ auth/auth.go
@@ -0,0 +1,4 @@
+// Package auth is a auth server package which will handle the login request
+// user will receive a JWT Token once sucessfull login. JWT token will
+// contain basic information of the user which depend on application call.
+package auth
---
+++ server/handlers.go
@@ -0,0 +1,27 @@
+package server
+
+import "net/http"
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func refreshHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func userDetailHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func priorityHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func studentConfirmHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func teacherToggleConfirmHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func capacityHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func allocationHandler(w http.ResponseWriter, r *http.Request) {
+}
