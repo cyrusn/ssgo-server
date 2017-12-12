3cb4d4daef1e5aee1e855b10ce5577471c8fe4fd
cyrusn <cyrusncy@gmail.com>
2017-12-12 20:02:18 +0800

    add HTTPLogger

--- helper/helper.go
+++ helper/helper.go
@@ -1,9 +1,19 @@
 package helper
 
 import (
+	"log"
+	"net/http"
 	"reflect"
 	"testing"
 )
+
+// HTTPLogger is a simple logger to log the request info
+func HTTPLogger(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
 
 // ExpectError is a testing tool, it used to test for error handling
 func ExpectError(name string, t *testing.T, f func()) {
