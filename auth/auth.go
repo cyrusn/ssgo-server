acf4255965c70ecad7a03dcb98006b424ec02645
cyrusn <cyrusncy@gmail.com>
2017-12-30 21:44:53 +0800

    complete auth_test

--- auth/auth.go
+++ auth/auth.go
@@ -4,33 +4,83 @@
 package auth
 
 import (
+	"context"
+	"errors"
+	"net/http"
+
+	helper "github.com/cyrusn/goHTTPHelper"
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
-type User struct {
-	Username string
-	Role     string
+// Claim store information of JWT token
+type Claim struct {
+	Payload interface{}
 	jwt.StandardClaims
 }
 
-// Validate validate JWT Token
-func Validate(jwtToken string) error {
-	return nil
+type privateKey []byte
+type contextKey string
+
+func (c contextKey) String() string {
+	return "auth context key " + string(c)
 }
 
-// Validate create JWT token for user
-func GenerateJWTToken(u *User) (string, error) {
-	key := "hello world"
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, u)
-	tokenString, err := token.SignedString(key)
+var (
+	key = []byte("secret")
+	// ContextKeyClaim is the key in context for retreive the information of claim
+	ContextKeyClaim = contextKey("claims")
+)
 
+// SetPrivateKey set the privateKey for authentication
+func SetPrivateKey(newKey []byte) {
+	key = newKey
+}
+
+// Required is a middleware for http.Handler that need right to access
+func Required(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		jwtToken := r.Header.Get("jwt-token")
+		if jwtToken == "" {
+			errCode := http.StatusForbidden
+			helper.PrintError(w, errors.New("Token not found"), errCode)
+			return
+		}
+
+		token, err := jwt.Parse(jwtToken, func(token *jwt.Token) (interface{}, error) {
+			return key, nil
+		})
+
+		errCode := http.StatusUnauthorized
+		if err != nil {
+			helper.PrintError(w, err, errCode)
+		}
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			ctx := context.WithValue(r.Context(), ContextKeyClaim, claims)
+			req := r.WithContext(ctx)
+			handler.ServeHTTP(w, req)
+		} else {
+			helper.PrintError(w, errors.New("Invalid Token"), errCode)
+		}
+	})
+}
+
+// Authoriser is an interface the auth func
+type Authoriser interface {
+	Authorise(username string, password string) error
+}
+
+func (claim *Claim) generateJWTToken(privateKey []byte) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+	return token.SignedString(privateKey)
+}
+
+// GetToken send jwt token if the Basic Auth success
+func (claim *Claim) GetToken(auth Authoriser, username, password string) (string, error) {
+	err := auth.Authorise(username, password)
 	if err != nil {
 		return "", err
 	}
 
-	return tokenString, nil
+	return claim.generateJWTToken(key)
 }
-
-func Sign() {}
-
-func ParseJWT() {}
--- auth/auth_test.go
+++ auth/auth_test.go
@@ -1,17 +1,166 @@
 package auth_test
 
 import (
-	"fmt"
+	"context"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 
+	helper "github.com/cyrusn/goHTTPHelper"
+	"github.com/cyrusn/goTestHelper"
 	"github.com/cyrusn/ssgo/auth"
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
-func TestMain(t *testing.T) {
-	user := auth.User{Username: "lpcyn"}
-	token, err := auth.GenerateJWTToken(&user)
-	if err != nil {
-		t.Fatal(err)
+type User struct {
+	Username string
+	Role     string
+	Password string
+}
+
+type Credential struct {
+	Username string
+	Password string
+}
+
+type TestModel struct {
+	Name string
+	*User
+	*Credential
+}
+
+var testModels = []*TestModel{
+	&TestModel{
+		Name: "Success Case",
+		User: &User{
+			Username: "lpteacher1",
+			Password: "abc123",
+			Role:     "TEACHER",
+		},
+		Credential: &Credential{
+			Username: "lpteacher1",
+			Password: "abc123",
+		},
+	},
+	&TestModel{
+		Name: "Incorrect Login",
+		User: &User{
+			Username: "lpstudent1",
+			Password: "def456",
+			Role:     "STUDENT",
+		},
+		Credential: &Credential{
+			Username: "lpstudent1",
+			Password: "def123",
+		},
+	},
+	&TestModel{
+		Name: "Forbidden Role",
+		User: &User{
+			Username: "lpstudent2",
+			Password: "ghi789",
+			Role:     "STUDENT",
+		},
+		Credential: &Credential{
+			Username: "lpstudent2",
+			Password: "ghi789",
+		},
+	},
+}
+
+func (m *TestModel) Authorise(username, password string) error {
+	for _, m := range testModels {
+		if m.User.Username == username && m.User.Password == password {
+			return nil
+		}
 	}
-	fmt.Println(token)
+	return errors.New("Unauthorised")
+}
+
+type payload struct {
+	Username string
+	Role     string
+}
+
+const (
+	AuthorizedRole = "TEACHER"
+)
+
+var (
+	privateKey = []byte("hello world")
+	tokens     = []string{}
+)
+
+func init() {
+	auth.SetPrivateKey(privateKey)
+}
+
+func TestMain(t *testing.T) {
+	expireToken := time.Now().Add(time.Minute * 30).Unix()
+	handler := http.HandlerFunc(simpleHandler)
+	for _, m := range testModels {
+		t.Run(m.Name, func(t *testing.T) {
+
+			claim := auth.Claim{
+				Payload: payload{Username: m.User.Username, Role: m.User.Role},
+				StandardClaims: jwt.StandardClaims{
+					ExpiresAt: expireToken,
+				},
+			}
+			password := m.Credential.Password
+			token, err := claim.GetToken(m, m.User.Username, password)
+
+			if m.User.Password == password {
+				assert.OK(t, err)
+			} else {
+				assert.Panic("Fail to login", t, func() {
+					panic(err)
+				})
+			}
+			w := httptest.NewRecorder()
+			req := httptest.NewRequest("GET", "/auth/", nil)
+			req.Header.Set("jwt-token", token)
+			authorizedHandler := auth.Required(handler)
+			authorizedHandler.ServeHTTP(w, req)
+			resp := w.Result()
+			body, err := ioutil.ReadAll(resp.Body)
+			assert.OK(t, err)
+
+			if m.User.Role == AuthorizedRole {
+				assert.Equal(string(body), "secret message", t)
+			} else {
+				assert.NotEqual(string(body), "secret message", t)
+			}
+		})
+	}
+}
+
+func parseRole(ctx context.Context) (string, error) {
+	m := ctx.Value(auth.ContextKeyClaim).(jwt.MapClaims)
+	payload, ok := m["Payload"].(map[string]interface{})
+	if !ok {
+		return "", errors.New("Claim not found in context")
+	}
+
+	result, ok := payload["Role"].(string)
+	if !ok {
+		return "", errors.New("Invalid payload")
+	}
+	return result, nil
+}
+
+func simpleHandler(w http.ResponseWriter, r *http.Request) {
+	errCode := http.StatusUnauthorized
+	role, err := parseRole(r.Context())
+	if err != nil {
+		helper.PrintError(w, err, errCode)
+	}
+	if role == AuthorizedRole {
+		w.Write([]byte("secret message"))
+		return
+	}
+	helper.PrintError(w, errors.New("User not allow"), errCode)
 }
